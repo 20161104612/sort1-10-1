0220589d0f530341dc80c723b714d070f741dd0b
20161104612 <643024265@qq.com>
2018-09-19 11:41:24 +0800

    1
    1

--- main.swift
+++ main.swift
@@ -1,9 +1,29 @@
 //
 //  main.swift
-//  sort1-10
+//  l02
 //
-//  Created by 20161104612 on 2018/9/19.
+//  Created by 20161104612 on 18/9/7.
 //  Copyright © 2018年 20161104612. All rights reserved.
 //
 
+import Foundation
+
+var a = 1
+a = 10
+var b = 2
+let c = a+b
+
+var sum = 0
+for i in 0...100{
+    sum=sum+i;
+}
+
+for x in 0...10{
+    print("\(x)",terminator:" ")
+}
+
+print()
+
+print(sum)
+print(c)
 
